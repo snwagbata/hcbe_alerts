a7d71f20dbcf85d2f03b0ea4a2fedb5b9898263d
Somtochukwu Nwagbata <healer2030@gmail.com>
2020-02-23 21:13:03 -0500

    added new settings

--- lib/routes/settings.dart
+++ lib/routes/settings.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hcbe_alerts/models/state.dart';
 import 'package:hcbe_alerts/routes/landing.dart';
 import 'package:hcbe_alerts/services/state_widget.dart';
+import 'package:location_permissions/location_permissions.dart';
 
 class SettingsPage extends StatefulWidget {
   _SettingsPageState createState() => _SettingsPageState();
@@ -9,9 +10,31 @@ class SettingsPage extends StatefulWidget {
 
 class _SettingsPageState extends State<SettingsPage> {
   StateModel appState;
+  PermissionStatus _status;
   @override
   void initState() {
     super.initState();
+    LocationPermissions().checkPermissionStatus().then(_updateStatus);
+  }
+
+  void _updateStatus(PermissionStatus status) {
+    if (status != _status) {
+      setState(() {
+        _status = status;
+      });
+    }
+  }
+
+  void _askPermission() async {
+    await LocationPermissions().requestPermissions().then(_onStatusRequested);
+  }
+
+  void _onStatusRequested(PermissionStatus value) {
+    if (value != PermissionStatus.granted) {
+      LocationPermissions().openAppSettings();
+    } else {
+      _updateStatus(value);
+    }
   }
 
   Widget build(BuildContext context) {
@@ -24,12 +47,43 @@ class _SettingsPageState extends State<SettingsPage> {
     } else {
       return Scaffold(
         appBar: AppBar(
-          centerTitle: true,
           elevation: 5.0,
           title: Text('Settings'),
         ),
         backgroundColor: Colors.white,
-        body: CircularProgressIndicator(),
+        body: SafeArea(
+          child: ListView(
+            children: <Widget>[
+              ListTile(
+                contentPadding: EdgeInsets.zero,
+                title: Text("Account"),
+                subtitle: Text("Privacy, security, change school code."),
+              ),
+              Divider(),
+              ListTile(
+                contentPadding: EdgeInsets.zero,
+                title: Text("Location"),
+                subtitle: Text("Location access permissions"),
+                onTap: () async {
+                  await LocationPermissions().openAppSettings();
+                },
+              ),
+              Divider(),
+              ListTile(
+                contentPadding: EdgeInsets.zero,
+                title: Text("Help"),
+                subtitle: Text("FAQ, contact us, privacy policy"),
+              ),
+              Divider(),
+              ListTile(
+                //use aboutDialog ontap
+                title: Text("Copyright ©2020 NS Tech"),
+                subtitle: Text("Somtochukwu Nwagbata"),
+              ),
+              Divider(),
+            ],
+          ),
+        ),
       );
     }
   }
