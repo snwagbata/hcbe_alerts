a2c781f9c7a866ad8ea493974c7cd9d194dc1153
Somtochukwu Nwagbata <healer2030@gmail.com>
2020-02-23 21:22:40 -0500

    alert trigger process ui changes

--- lib/routes/default.dart
+++ lib/routes/default.dart
@@ -1,14 +1,17 @@
+import 'dart:async';
 import 'dart:io';
 
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hcbe_alerts/models/state.dart';
 import 'package:hcbe_alerts/services/alerts.dart';
 import 'package:hcbe_alerts/services/state_widget.dart';
-import 'package:hcbe_alerts/widgets/alert_dialog.dart';
+import 'package:hcbe_alerts/widgets/distress_dialog.dart';
 import 'package:hcbe_alerts/widgets/drawer.dart';
 import 'package:hcbe_alerts/widgets/loading.dart';
+import 'package:location_permissions/location_permissions.dart';
 
 class DefaultPage extends StatefulWidget {
   _DefaultPageState createState() => _DefaultPageState();
@@ -16,6 +19,7 @@ class DefaultPage extends StatefulWidget {
 
 class _DefaultPageState extends State<DefaultPage> {
   StateModel appState;
+  PermissionStatus _status;
   bool _loadingVisible = false;
   String _currentCode = "";
   String _currentCodeImg = "assets/code_green.png";
@@ -24,6 +28,7 @@ class _DefaultPageState extends State<DefaultPage> {
   Widget build(BuildContext context) {
     appState = StateWidget.of(context).state;
     final schoolId = appState?.user?.school ?? '';
+    final userId = appState?.user?.getUserId();
 
     if (appState.isLoading) {
       _loadingVisible = true;
@@ -31,26 +36,48 @@ class _DefaultPageState extends State<DefaultPage> {
       _loadingVisible = false;
     }
 
+    Future<void> _confirmAlertTrigger(
+        BuildContext context, String title, String alert) async {
+      final bool didTriggerAlert = await DistressAlertDialog(
+        title: title,
+        cancelActionText: 'Cancel',
+        defaultActionText: 'Initiate alert',
+        schoolId: schoolId,
+        alert: alert,
+      ).show(context);
+      if (didTriggerAlert != false) {
+        await Alerts.fire(schoolId, alert, userId);
+      }
+    }
+
     final activeIntruder = RaisedButton(
-      onPressed: () {displayDialog(context, "Active Intruder", "intruder", schoolId, Colors.grey);},
+      onPressed: () {
+        _confirmAlertTrigger(context, "Active Intruder", "intruder");
+      },
       padding: EdgeInsets.symmetric(vertical: 12),
       color: Colors.grey,
       child: Text("Active Intruder"),
     );
     final codeRed = RaisedButton(
-      onPressed: () {displayDialog(context, "Code Red", "red", schoolId, Colors.red);},
+      onPressed: () {
+        _confirmAlertTrigger(context, "Code Red", "red");
+      },
       padding: EdgeInsets.symmetric(vertical: 12),
       color: Colors.red,
       child: Text("Code Red"),
     );
     final codeYellow = RaisedButton(
-      onPressed: () {displayDialog(context, "Code Yellow", "yellow", schoolId, Colors.yellowAccent);},
+      onPressed: () {
+        _confirmAlertTrigger(context, "Code Yellow", "yellow");
+      },
       padding: EdgeInsets.symmetric(vertical: 12),
       color: Colors.yellow,
       child: Text("Code Yellow"),
     );
     final codeBlue = RaisedButton(
-      onPressed: () {displayDialog(context, "Code Blue", "blue", schoolId, Colors.blue);},
+      onPressed: () {
+        _confirmAlertTrigger(context, "Code Blue", "blue");
+      },
       padding: EdgeInsets.symmetric(vertical: 12),
       color: Colors.blue,
       child: Text("Code Blue"),
@@ -62,11 +89,13 @@ class _DefaultPageState extends State<DefaultPage> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Center(
-              child: Text(
+              child: AutoSizeText(
                 _currentSchoolName,
                 style: Theme.of(context).textTheme.title.copyWith(
-                      fontSize: 24,
+                      fontSize: 22,
                     ),
+                minFontSize: 16,
+                maxLines: 2,
                 textAlign: TextAlign.center,
               ),
             ),
@@ -171,19 +200,29 @@ class _DefaultPageState extends State<DefaultPage> {
     );
   }
 
-  displayDialog(BuildContext context, String title, String alert,
-      String schoolId, Color color) async {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return DistressDialog(
-            title: title, alert: alert, schoolId: schoolId, color: color);
-      },
-    );
-  }
-
   @override
   void initState() {
     super.initState();
+    LocationPermissions().checkPermissionStatus().then(_updateStatus);
+  }
+
+  void _updateStatus(PermissionStatus status) {
+    if (status != _status) {
+      setState(() {
+        _status = status;
+      });
+    }
+  }
+
+  void _askPermission() async {
+    await LocationPermissions().requestPermissions().then(_onStatusRequested);
+  }
+
+  void _onStatusRequested(PermissionStatus value) {
+    if (value != PermissionStatus.granted) {
+      LocationPermissions().openAppSettings();
+    } else {
+      _updateStatus(value);
+    }
   }
 }
