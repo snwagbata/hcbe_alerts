f7eb7477a8cbb3e68088d564362d89fe7602026e
Somtochukwu Nwagbata <healer2030@gmail.com>
2020-02-23 21:19:45 -0500

    Alerts.fire complete

--- lib/services/alerts.dart
+++ lib/services/alerts.dart
@@ -1,10 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:hcbe_alerts/models/state.dart';
-import 'package:hcbe_alerts/models/user.dart';
-import 'package:hcbe_alerts/services/firebase.dart';
-import 'package:hcbe_alerts/services/state_widget.dart';
+import 'package:hcbe_alerts/models/distress.dart';
+import 'package:location/location.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Alerts {
+  /// Returns the school alert state
   static Future<String> getCurrentAlert(String schoolId) async {
     String curCode = "";
     var doc = await Firestore.instance.document("schools/$schoolId").get();
@@ -13,9 +13,7 @@ class Alerts {
   }
 
   static Future<String> getCurrentAlertName(String schoolId) async {
-    String curCode = "";
-    var doc = await Firestore.instance.document("schools/$schoolId").get();
-    curCode = doc.data["schoolAlertState"];
+    String curCode = await getCurrentAlert(schoolId);
 
     switch (curCode) {
       case "red":
@@ -41,11 +39,63 @@ class Alerts {
   /// update db based on schoolId first
   /// then get location and everything required to create an alert object
   /// create alert object then create new alert under alert collection in db
-  static fire(String schoolId, String alertType) {}
+  static fire(String schoolId, String alertType, String userId) async {
+    Firestore.instance
+        .document("schools/$schoolId")
+        .updateData({"schoolAlertState": alertType, "schoolAlertActive": true});
+    // get location and other prerequisites needed for the Distress object
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String _alertMessage;
+    if (prefs.getString('settings') != null) {
+      _alertMessage = prefs.getString('alertMessage');
+    } else {
+      _alertMessage = "";
+    }
+
+    Distress sendToDB = Distress(
+      active: true,
+      triggeredBy: userId,
+      distressId: null,
+      distressType: alertType,
+      schoolId: schoolId,
+      timeTriggered: new DateTime.now(),
+      message: _alertMessage,
+    );
+    Firestore.instance
+        .collection("alerts")
+        .add(sendToDB.toJson())
+        .then((docRef) async {
+      //update location
+      GeoPoint location = await _getLocation();
+      Firestore.instance
+          .collection("alerts")
+          .document(docRef.documentID)
+          .updateData({"location": location});
+
+//send fcm to all users with schoolId
+    });
+
+    await prefs.setString('alertMessage', "");
+  }
 
   /// Updates current active alert with message. Only works when current school alert is not `green`
-  /// 
-  static void updateAlert(String message) {}
+  ///
+  static void updateAlertMessages(String message) {}
+
+  /// Get location, return null if access denied
+  static Future<GeoPoint> _getLocation() async {
+    //TODO: create new alert in db with loc off
+    Location location = new Location();
+    var pos = await location.getLocation();
+    GeoPoint _curLoc;
+    if (pos is Error) {
+      _curLoc = null;
+    } else {
+      _curLoc = GeoPoint(pos.latitude, pos.longitude);
+    }
+    return _curLoc;
+  }
 
   static Future<String> getAlertImgPath(String schoolId) async {
     String curCode = "";
