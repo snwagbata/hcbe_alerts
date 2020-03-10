21d205ad231b613d56c74646d828481f6b802f59
Somtochukwu Nwagbata <healer2030@gmail.com>
2020-03-10 16:56:37 -0400

    updated setings page

--- lib/routes/settings.dart
+++ lib/routes/settings.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:hcbe_alerts/models/state.dart';
 import 'package:hcbe_alerts/routes/landing.dart';
 import 'package:hcbe_alerts/services/state_widget.dart';
-import 'package:location_permissions/location_permissions.dart';
+import 'package:hcbe_alerts/widgets/flushbar.dart';
+import 'package:hcbe_alerts/widgets/platform_alert_dialog.dart';
+import 'package:settings_ui/settings_ui.dart';
+import 'package:app_settings/app_settings.dart';
+import 'package:flutter/services.dart';
 
 class SettingsPage extends StatefulWidget {
   _SettingsPageState createState() => _SettingsPageState();
@@ -10,31 +14,9 @@ class SettingsPage extends StatefulWidget {
 
 class _SettingsPageState extends State<SettingsPage> {
   StateModel appState;
-  PermissionStatus _status;
   @override
   void initState() {
     super.initState();
-    LocationPermissions().checkPermissionStatus().then(_updateStatus);
-  }
-
-  void _updateStatus(PermissionStatus status) {
-    if (status != _status) {
-      setState(() {
-        _status = status;
-      });
-    }
-  }
-
-  void _askPermission() async {
-    await LocationPermissions().requestPermissions().then(_onStatusRequested);
-  }
-
-  void _onStatusRequested(PermissionStatus value) {
-    if (value != PermissionStatus.granted) {
-      LocationPermissions().openAppSettings();
-    } else {
-      _updateStatus(value);
-    }
   }
 
   Widget build(BuildContext context) {
@@ -45,14 +27,84 @@ class _SettingsPageState extends State<SettingsPage> {
             appState.settings == null)) {
       return LandingPage();
     } else {
+      Future<void> _signOut(BuildContext context) async {
+    try {
+      bar("Signed out", 2, context);
+      StateWidget.of(context).logOutUser();
+    } on PlatformException {
+      bar('Sign Out failed', 5, context);
+    }
+  }
+
+  Future<void> _confirmSignOut(BuildContext context) async {
+    final bool didRequestSignOut = await PlatformAlertDialog(
+      title: 'Sign Out',
+      content: 'Are you sure you want to sign out?',
+      cancelActionText: 'Cancel',
+      defaultActionText: 'Sign Out',
+    ).show(context);
+    if (didRequestSignOut == true) {
+      _signOut(context);
+    }
+  }
+
       return Scaffold(
         appBar: AppBar(
           elevation: 5.0,
           title: Text('Settings'),
         ),
-        backgroundColor: Colors.white,
         body: SafeArea(
-          child: ListView(
+          child: SettingsList(
+        sections: [
+          SettingsSection(
+            title: 'Common',
+            tiles: [
+              /*SettingsTile(
+                title: 'Language',
+                subtitle: 'English',
+                leading: Icon(Icons.language),  will implement spanish later
+                onTap: () {
+                  Navigator.of(context);
+                },
+              ),*/
+            ],
+          ),
+          SettingsSection(
+            title: 'Account',
+            tiles: [
+              SettingsTile(title: 'Email', leading: Icon(Icons.email)),
+              SettingsTile(title: 'Sign out', leading: Icon(Icons.exit_to_app), onTap: () {_confirmSignOut(context);},),
+            ],
+          ),
+          SettingsSection(
+            title: 'Security',
+            tiles: [
+              SettingsTile(
+                title: 'Location permission',
+                leading: Icon(Icons.lock),
+                onTap: () {
+                  AppSettings.openLocationSettings();
+                },
+              ),
+              SettingsTile(
+                title: 'Change password',
+                leading: Icon(Icons.lock),
+              ),
+            ],
+          ),
+          SettingsSection(
+            title: 'Misc',
+            tiles: [
+              SettingsTile(
+                  title: 'Terms of Service', leading: Icon(Icons.description)),
+              SettingsTile(
+                  title: 'Open source licenses',
+                  leading: Icon(Icons.collections_bookmark)),
+            ],
+          )
+        ],
+      ),
+          /*child: ListView(
             children: <Widget>[
               ListTile(
                 contentPadding: EdgeInsets.zero,
@@ -82,7 +134,7 @@ class _SettingsPageState extends State<SettingsPage> {
               ),
               Divider(),
             ],
-          ),
+          ),*/
         ),
       );
     }
