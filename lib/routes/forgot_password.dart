5c0589b1f01dda1c9787a8cf80a34615ef3d7a3f
Somtochukwu Nwagbata <healer2030@gmail.com>
2020-03-11 18:49:20 -0400

    fixed issue with border radius for email input

--- lib/routes/forgot_password.dart
+++ lib/routes/forgot_password.dart
@@ -53,7 +53,6 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
         ),
         labelText: 'Email',
         contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
       ),
     );
 
