3528088b449d9d6365d527930da58f425388070b
Somtochukwu Nwagbata <healer2030@gmail.com>
2020-06-20 11:47:44 -0400

    refactored

---
+++ lib/services/location.dart
@@ -0,0 +1,26 @@
+import 'package:location/location.dart';
+
+class LocationInit {
+  static initLocation() async {
+    Location location = new Location();
+
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+  }
+}
