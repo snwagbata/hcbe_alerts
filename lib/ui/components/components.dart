65fd31f4dbd178139174737c9ebd88a04aee6afe
Somtochukwu Nwagbata <healer2030@gmail.com>
2020-06-20 18:04:01 -0400

    export fille for components

---
+++ lib/ui/components/components.dart
@@ -0,0 +1,13 @@
+export 'loading_screen.dart';
+export 'primary_button.dart';
+export 'label_button.dart';
+export 'logo_graphic_header.dart';
+export 'avatar.dart';
+export 'distress_dialog.dart';
+export 'code_button.dart';
+export 'alert_card.dart';
+export 'distress_dialog_admin.dart';
+export 'platform_alert_dialog.dart';
+export 'about_dialog.dart';
+export 'alert_message.dart';
+export 'alert_detail_chip.dart';
